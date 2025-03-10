f6a350785dd0ccf76f9af928033be021fa9d12f2
khalidzia31016 <malikawan16830@gmail.com>
2025-03-10 06:15:50 +0500

    only readme here

---
+++ android/app/src/main/kotlin/com/example/mini_uni/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mini_uni
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/read_me.dart
@@ -0,0 +1,50 @@
+// Mini University here
+// must be using bloc
+// and MVVM
+//
+//
+// 1.create a table in SQLITE for the following scenario
+//
+// students info
+// sName, sId, sAge, sPic, sPhone, semester, courses enrolled, sDepartment, sProgram, sSex
+//
+// teachers info
+// tName, tDepartment, tcourses enrolleds, tPhone, tAge, tSex, tPic
+//
+// staff info
+// stName, stPhone, stAge, stSex, stPic, stDuty
+//
+//
+// invigilations
+// name, lr, time, day
+//
+//
+// 2. Teachers and Staff can generate their duty info card
+//
+// 3. Teachers and staff can get notified with mobiel sms for mentionig theri duties
+//
+// 4. Students can generate their Admit card as well
+//
+// 5. students must be 20
+//
+// male, female
+// MS BS PHD
+//
+// 6. submit their form using sID to generate their Admit card
+// Admit card can show their name, id, papers, lr, day, time etc
+//
+// 7. also generate the Datesheet for all first
+//
+// 8. Offer some courses for MS, Phd, BS etc
+//
+// 9. one student can enroll in many courses, two students can be enrolled in similar courses
+//
+// 10. students can drop their courses as well
+//
+// 11. students can see their result
+//
+// 12. students can enroll in new courses
+//
+//
+//
+//
